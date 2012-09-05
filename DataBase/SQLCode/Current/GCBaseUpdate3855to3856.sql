5a3794d93f0eac7de1a6d8e4c6fa3fd7f66512ef
numbnp@gmail.com <numbnp@gmail.com@6e38c052-cabe-11de-ba9f-01e9bd544a36>
2012-09-05 15:17:38 +0000

    + к 156

---
+++ DataBase/SQLCode/Current/GCBaseUpdate3855to3856.sql
@@ -0,0 +1,45 @@
+USE GameClass
+GO
+
+/* -----------------------------------------------------------------------------
+                               Правим косяк с доступом к ReportCurrent
+----------------------------------------------------------------------------- */
+
+GRANT  EXECUTE  ON [dbo].[ReportCurrent]  TO [public]
+GO
+
+
+/* -----------------------------------------------------------------------------
+                               Чистим AutoUpdate
+----------------------------------------------------------------------------- */
+
+
+DELETE FROM [GameClass].[dbo].[AutoUpdate] WHERE 1=1;
+GO
+
+/* -----------------------------------------------------------------------------
+	           Добавление права на частичную сдачу
+----------------------------------------------------------------------------- */
+
+
+IF NOT EXISTS (SELECT * FROM [dbo].[functions] WHERE [id] = 37)
+BEGIN
+	INSERT INTO [functions]([id],[name]) VALUES(37,N'fnCompBackPartMoney')
+
+	INSERT INTO [functionsrights]([idFunctions],[idUsersGroup]) VALUES(37,1)
+	INSERT INTO [functionsrights]([idFunctions],[idUsersGroup]) VALUES(37,2)
+	INSERT INTO [functionsrights]([idFunctions],[idUsersGroup]) VALUES(37,3)
+END
+GO
+
+
+
+
+
+/* -----------------------------------------------------------------------------
+                               UPDATE Version
+----------------------------------------------------------------------------- */
+#UPDATE Registry SET [value]='3.85.6' WHERE [key]='BaseVersion'
+#GO
+
+
