bcc59f107cd7fea8952b166545b991004f002915
Напад Максим Александрович <maxim@napad.ru>
2021-02-03 21:15:03 +0300

    Update synedit

--- Components/SynEdit/Source/SynEditTypes.pas
+++ Components/SynEdit/Source/SynEditTypes.pas
@@ -79,21 +79,28 @@ type
 
   //todo: better field names. CharIndex and LineIndex?
   TBufferCoord = record
-    Char: integer;
-    Line: integer;
+    Char: Integer;
+    Line: Integer;
+    {$IFDEF SYN_COMPILER_10_UP}
+    class operator Equal(a, b: TBufferCoord): Boolean;
+    {$ENDIF}
   end;
 
   // Codehunter patch: added TBufferBlock
   TBufferBlock = record
-    BeginLine,
-    BeginChar,
-    EndLine,
-    EndChar: Integer;
+    BeginLine, BeginChar: Integer;
+    EndLine, EndChar: Integer;
+    {$IFDEF SYN_COMPILER_10_UP}
+    class operator Equal(a, b: TBufferBlock): Boolean;
+    {$ENDIF}
   end;
 
   TDisplayCoord = record
-    Column: integer;
-    Row: integer;
+    Column: Integer;
+    Row: Integer;
+    {$IFDEF SYN_COMPILER_10_UP}
+    class operator Equal(a, b: TDisplayCoord): Boolean;
+    {$ENDIF}
   end;
 
 function DisplayCoord(AColumn, ARow: Integer): TDisplayCoord;
@@ -112,5 +119,31 @@ begin
   Result.Char := AChar;
   Result.Line := ALine;
 end;
+
+{$IFDEF SYN_COMPILER_10_UP}
+
+{ TBufferCoord }
+
+class operator TBufferCoord.Equal(a, b: TBufferCoord): Boolean;
+begin
+  Result := (a.Char = b.Char) and (a.Line = b.Line);
+end;
+
+{ TBufferBlock }
+
+class operator TBufferBlock.Equal(a, b: TBufferBlock): Boolean;
+begin
+  Result := (a.BeginLine = b.BeginLine) and (a.BeginChar = b.BeginChar) and
+    (a.EndLine = b.EndLine) and (a.EndChar = b.EndChar);
+end;
+
+{ TDisplayCoord }
+
+class operator TDisplayCoord.Equal(a, b: TDisplayCoord): Boolean;
+begin
+  Result := (a.Row = b.Row) and (a.Column = b.Column);
+end;
+
+{$ENDIF}
 
 end.
